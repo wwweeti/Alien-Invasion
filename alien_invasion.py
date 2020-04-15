c3f7f34e446f49d7d1adb68d4235d2058dcdcaba
BuildTools <unconfigured@null.spigotmc.org>
2020-04-15 15:50:00 +0300

    Refactoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,25 +4,29 @@ import pygame
 from settings import Settings
 from ship import Ship
 
+
 class AlienInvasion:
     def __init__(self):
         pygame.init()
-        self.settings=Settings()
+        self.settings = Settings()
         self.ikkuna = pygame.display.set_mode((self.settings.leveys, self.settings.korkeus))
-        self.ship=Ship(self)
+        self.ship = Ship(self)
         pygame.display.set_caption("Alien invasion")
-
 
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            self.ikkuna.fill(self.settings.tauluväri)
-            self.ship.blitme()
-            pygame.display.flip()
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
+    def _update_screen(self):
+        self.ikkuna.fill(self.settings.tauluväri)
+        self.ship.blitme()
+        pygame.display.flip()
 
 
 ai = AlienInvasion()
--- ship.py
+++ ship.py
@@ -1,13 +1,15 @@
 import pygame
 
-class Ship:
-    def __init__(self,ai_game):
 
-        self.ikkuna_ship=ai_game.ikkuna
-        self.kuva=pygame.image.load("images/ship.bmp")
-        self.kuva_kehys=self.kuva.get_rect()
-        self.ikkuna_kehys=ai_game.ikkuna.get_rect()
-        self.kuva_kehys.midbottom=self.ikkuna_kehys.midbottom
+class Ship:
+    def __init__(self, ai_game):
+        self.kuva = pygame.image.load("images/ship.bmp")
+        self.kuva_kehys = self.kuva.get_rect()
+
+        self.ikkuna_ship = ai_game.ikkuna
+        self.ikkuna_kehys = self.ikkuna_ship.get_rect()
+
+        self.kuva_kehys.midbottom = self.ikkuna_kehys.midbottom
 
     def blitme(self):
-        self.ikkuna_ship.blit(self.kuva,self.kuva_kehys)
+        self.ikkuna_ship.blit(self.kuva, self.kuva_kehys)
